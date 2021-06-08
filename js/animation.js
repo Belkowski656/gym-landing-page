358fc12adc76b80a71f31729dfeea288d1fb8a8e
Belkowski656 <hub.mezykprogramming@gmail.com>
2021-06-08 16:44:04 +0200

    add animation on scroll for all sections

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger);
+
 window.addEventListener('load', () => {
     const tl = gsap.timeline();
 
@@ -44,4 +46,56 @@ window.addEventListener('load', () => {
             y: 900,
             ease: "back.out(1.01)",
         }, 'start')
+})
+
+gsap.from(".about__title", {
+    scrollTrigger: '.about__title',
+    x: -600,
+    ease: "back.out(1.7)",
+    duration: .5,
+})
+
+gsap.from(".about__text", {
+    scrollTrigger: '.about__text',
+    x: 600,
+    ease: "back.out(1.7)",
+    duration: .5,
+})
+
+gsap.from(".about .btn", {
+    scrollTrigger: '.about .btn',
+    y: 600,
+    ease: "back.out(1.7)",
+    duration: .5,
+})
+
+gsap.from(".about__photo:nth-child(1)", {
+    scrollTrigger: '.about__photo:nth-child(1)',
+    x: -600,
+    ease: "back.out(1.7)",
+    duration: .5,
+})
+
+gsap.from(".about__photo:nth-child(2)", {
+    scrollTrigger: '.about__photo:nth-child(2)',
+    x: 600,
+    ease: "back.out(1.7)",
+    duration: .5,
+})
+
+gsap.from(".trainers__trainer", {
+    scrollTrigger: '.trainers__trainer',
+    y: 350,
+    opacity: 0,
+    ease: "back.out(1.7)",
+    duration: .5,
+    start: 'bottom mid'
+})
+
+gsap.from(".footer__box", {
+    scrollTrigger: '.footer__box',
+    y: 200,
+    ease: "back.out(1.7)",
+    duration: .5,
+
 })
